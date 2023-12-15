13d140ce1e7f04801aa1aa2def341cba8606cfcf
erentrk01 <68688967+erentrk01@users.noreply.github.com>
2023-12-15 20:46:28 +0300

    Initial commit

---
+++ lib/web_478_project-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class Web478ProjectStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'Web478ProjectQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
